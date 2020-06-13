a9dc5b34481a708e61eb9c8abfdc82f314e62996
Apollo Truong <apollotruong@gmail.com>
2020-06-12 20:57:59 -0700

    General implementation of algorithm partially done

--- sketch.js
+++ sketch.js
@@ -1,5 +1,18 @@
-var cols = 5;
-var rows = 5;
+function removeFromArray(arr,elt){
+    for(var i = arr.length-1; i >=0; i--){
+        if(arr[i] == elt) {
+            arr.splice(i,1);
+        }
+    }
+}
+
+function heuristic(a,b){
+    var d = dist(a.i,a.j,b.i,b.j);
+    return d;
+}
+
+var cols = 25;
+var rows = 25;
 var grid = new Array(cols)
 
 var openSet = [];
@@ -9,18 +22,37 @@ var end;
 var w,h;
 
 function Spot(i,j) {
-    this.x = i;
-    this.y = j;
+    this.i = i;
+    this.j = j;
     this.f = 0;
     this.g = 0;
     this.h = 0;
+    this.neighbors = [];
 
-    this.show = function() {
-        fill(255);
-        stroke(0);
-        rect(this.x * w,this.y * h, w-1, h-1);
+    this.show = function(col) {
+        fill(col);
+        noStroke();
+        rect(this.i * w,this.j * h, w-1, h-1);
     }
 
+    this.addNeighbors = function(grid) {
+        var i = this.i;
+        var j = this.j;
+        if(i < cols-1) {
+            this.neighbors.push(grid[i+1][j]);
+        }
+        
+        if(i > 0){
+            this.neighbors.push(grid[i-1][j]);
+        }
+
+        if(j < rows-1){
+            this.neighbors.push(grid[i][j+1]);
+        }
+        if(j > 0){
+            this.neighbors.push(grid[i][j-1]);
+        }
+    }
 }
 
 function setup() {
@@ -38,8 +70,16 @@ function setup() {
     for(var i = 0; i < cols; i++){
         for(var j = 0; j < rows; j++){
             grid[i][j] = new Spot(i,j);
+
         }
     }
+
+    for(var i = 0; i < cols; i++){
+        for(var j = 0; j < rows; j++){
+            grid[i][j].addNeighbors(grid);
+        }
+    }
+
 
     start = grid[0][0];
     end = grid[cols-1][rows-1];
@@ -54,6 +94,47 @@ function setup() {
 function draw() {
 
     if(openSet.length > 0){
+        var winner = 0;
+        for(var i = 0; i < openSet.length; i++){
+            if(openSet[i].f < openSet[winner].f){
+                winner = i;
+            }
+        }
+
+        var current = openSet[winner];
+
+
+        if(current == end) {
+            console.log("DONE!");
+        }
+
+        removeFromArray(openSet,current);
+        closedSet.push(current);
+        var neighbors = current.neighbors;
+        for( var i = 0; i < neighbors.length; i++){
+            var neighbor = neighbors[i];
+
+            if(!closedSet.includes(neighbor)){
+                var tempG = current.g + 1;
+
+                if(openSet.includes(neighbor)){
+                    if(tempG < neighbor.g){
+                        neighbor.g = tempG;
+                    }
+                } else{
+                    neighbor.g = tempG;
+                    openSet.push(neighbor);
+                }
+                neighbor.h = heuristic(neighbor,end);
+                neighbor.f = neighbor.g + neighbor.h;
+
+            }
+
+            
+        }
+
+
+
         
     } else {
         
@@ -62,8 +143,16 @@ function draw() {
 
     for(var i = 0; i < cols; i++){
         for(var j = 0; j < rows; j++){
-            grid[i][j].show();
+            grid[i][j].show(color(255));
         }
+    }
+
+    for (var i = 0; i < closedSet.length; i++){
+        closedSet[i].show(color(255,0,0));
+    }
+
+    for (var i = 0; i < openSet.length; i++){
+        openSet[i].show(color(0,255,0));
     }
 
 }
