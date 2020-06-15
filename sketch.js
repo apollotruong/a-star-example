a37d397bd92e34fad330b09524c7c75c6d2a6131
Apollo Truong <apollotruong@gmail.com>
2020-06-14 21:45:52 -0700

    Moving spot out from sketch

--- sketch.js
+++ sketch.js
@@ -8,8 +8,8 @@ function removeFromArray(arr, elt) {
 
 function heuristic(a, b) {
   var d = dist(a.i,a.j,b.i,b.j); //Euclidean Distance
-  // Manhattan distance
-//   var d = abs(a.i - b.i) + abs(a.j - b.j);
+// Manhattan distance
+//  var d = abs(a.i - b.i) + abs(a.j - b.j);
   return d;
 }
 
@@ -25,61 +25,8 @@ var w, h;
 var path = [];
 var nosolution = false;
 
-function Spot(i, j) {
-  this.i = i;
-  this.j = j;
-  this.f = 0;
-  this.g = 0;
-  this.h = 0;
-  this.neighbors = [];
-  this.previous = undefined;
-  this.wall = false;
-
-  if (random(1) < 0.4) {
-    this.wall = true;
-  }
-
-  this.show = function (col) {
-    fill(col);
-    if (this.wall) {
-      fill(0);
-    }
-    noStroke();
-    rect(this.i * w, this.j * h, w - 1, h - 1);
-  };
-
-  this.addNeighbors = function (grid) {
-    var i = this.i;
-    var j = this.j;
-    if (i < cols - 1) {
-      this.neighbors.push(grid[i + 1][j]);
-    }
-    if (i > 0) {
-      this.neighbors.push(grid[i - 1][j]);
-    }
-    if (j < rows - 1) {
-      this.neighbors.push(grid[i][j + 1]);
-    }
-    if (j > 0) {
-      this.neighbors.push(grid[i][j - 1]);
-    }
-    if (i > 0 && j > 0) {
-      this.neighbors.push(grid[i - 1][j - 1]);
-    }
-    if (i < cols - 1 && j > 0) {
-      this.neighbors.push(grid[i + 1][j - 1]);
-    }
-    if (i > 0 && j < rows - 1) {
-      this.neighbors.push(grid[i - 1][j + 1]);
-    }
-    if (i < cols - 1 && j < rows - 1) {
-      this.neighbors.push(grid[i + 1][j + 1]);
-    }
-  };
-}
-
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
   console.log("A*");
   w = width / cols;
   h = height / rows;
